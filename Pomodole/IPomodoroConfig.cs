f6d5f989ba87ae0208cd9da4792351d70cc132ba
Mojopon <tyamuro@gmail.com>
2015-12-03 17:16:31 +0900

    first commit
    first commit

---
+++ Pomodole/IPomodoroConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodole
+{
+    public interface IPomodoroConfig
+    {
+        int TaskTime { get; }
+        int BreakTime { get; }
+        int RepeatTime { get; }
+        int LongBreakTime { get; }
+    }
+}
---
+++ PomodoleTest/CountdownTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Pomodole;
+
+namespace PomodoleTest
+{
+    [TestFixture]
+    public class CountdownTest
+    {
+        Countdown countdown;
+        [SetUp]
+        public void CreateCountdown()
+        {
+            // initialize with start minute
+            countdown = new Countdown(10);
+        }
+
+        [Test]
+        public void ShouldProgressTimeUntilTimerEnd()
+        {
+            countdown.Tick();
+            Assert.AreEqual(59, countdown.GetSecond());
+            Assert.AreEqual(9, countdown.GetMinute());
+
+            countdown = new Countdown(1);
+            for (int timeLeft = 59; timeLeft > 0; timeLeft--)
+            {
+                countdown.Tick();
+                Assert.AreEqual(timeLeft, countdown.GetSecond());
+                Assert.IsFalse(countdown.CountdownEnd);
+            }
+            countdown.Tick();
+            Assert.AreEqual(0, countdown.GetSecond());
+            Assert.AreEqual(0, countdown.GetMinute());
+            Assert.IsTrue(countdown.CountdownEnd);
+        }
+
+        [Test]
+        public void ShouldResetTime()
+        {
+            countdown.Tick();
+            Assert.AreEqual(59, countdown.GetSecond());
+            Assert.AreEqual(9, countdown.GetMinute());
+            countdown.Reset();
+            Assert.AreEqual(0, countdown.GetSecond());
+            Assert.AreEqual(10, countdown.GetMinute());
+        }
+    }
+}
